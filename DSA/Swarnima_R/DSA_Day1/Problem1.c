16f149ec1443eef17f3b78b3e5fbaee73d1919ef
Swarnima <124855325+oringejooz@users.noreply.github.com>
2024-06-22 03:41:38 +0530

    new file:   .vscode/tasks.json 	modified:   DSA/Swarnima_R/DSA_Day1/Problem1.c 	new file:   DSA/Swarnima_R/DSA_Day1/Problem1.exe 	modified:   DSA/Swarnima_R/DSA_Day1/Problem2.c 	new file:   DSA/Swarnima_R/READ.md 	new file:   Web-Dev/Swarnima_R/READ.md 	new file:   Web-Dev/Swarnima_R/WebDev_Day1/Problem1.html 	new file:   Web-Dev/Swarnima_R/WebDev_Day1/Problem2.html 	new file:   Web-Dev/Swarnima_R/WebDev_Day1/Problem3.html 	new file:   Web-Dev/Swarnima_R/WebDev_Day1/Problem4.html

--- DSA/Swarnima_R/DSA_Day1/Problem1.c
+++ DSA/Swarnima_R/DSA_Day1/Problem1.c
@@ -19,11 +19,54 @@ The number of valid ways to divide the bar.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
+int countSegments(int *candy,int n, int day, int month);
 
+int main() {
+    int n, day, month;
 
-int main()
-{
+    printf("Enter the number of elements in the candy array = ");
+    scanf("%d", &n);
 
-return 0;
+    int* candy = (int*)malloc(n * sizeof(int));
+
+    if (candy == NULL) {
+        printf("NO MEMORY ALLOCATED\n");
+        return 1;
+    }
+
+    printf("Enter the elements of the candy array = ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &candy[i]);
+    }
+
+    printf("Enter Birthday = ");
+    scanf("%d", &day);
+    printf("Enter Birth month = ");
+    scanf("%d", &month);
+
+    int result = countSegments(candy,n, day, month);
+    printf("Number of valid segments = %d\n", result);
+
+    free(candy);
+
+    return 0;
 }
+
+int countSegments(int *candy,int n, int day, int month) {
+    int count = 0;
+
+    for (int i = 0; i <= n - month; i++) {
+        int sum = 0;
+        for (int j = 0; j < month; j++) {
+            sum += candy[i + j];
+        }
+        if (sum == day) {
+            count += 1;
+        }
+    }
+
+    return count;
+}
+
