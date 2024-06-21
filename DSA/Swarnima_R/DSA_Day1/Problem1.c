ecacb4e37d9b273bf29406f1ad4e4bc4aaa6ae62
Swarnima <124855325+oringejooz@users.noreply.github.com>
2024-06-22 02:26:06 +0530

    Commit 1

---
+++ DSA/Swarnima_R/DSA_Day1/Problem1.c
@@ -0,0 +1,29 @@
+/*
+Swarnima Bisht
+500105696
+
+Question 1:
+
+Rohan and Raju have a candy where each square has a number. Rohan wants to find out how many ways she can break the bar into a contiguous segment such that:
+
+The segment length matches Raju's birth month.
+The sum of the numbers in the segment equals Raju's birth day.
+Inputs:
+An array of integers representing the chocolate squares.
+
+Two integers representing Raju's birth day and birth month.
+
+Output:
+The number of valid ways to divide the bar.
+
+*/
+
+#include <stdio.h>
+
+
+
+int main()
+{
+
+return 0;
+}
---
+++ DSA/Swarnima_R/DSA_Day1/Problem2.c
@@ -0,0 +1,24 @@
+/*
+Swarnima Bishrt
+500105696
+
+Given an array of integers and an integer k, determine the number of (i, j) pairs where i < j and the sum of the pair is divisible by k.
+
+Inputs:
+An array of integers.
+
+An integer k.
+
+Output:
+The number of valid (i, j) pairs.
+
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+
+return 0;
+}
