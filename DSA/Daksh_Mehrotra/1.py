9312942069425d496bd52d4661765fa1c7484941
DakshMehrotra <148863185+DakshMehrotra@users.noreply.github.com>
2024-06-23 12:47:03 +0530

    Add files via upload
    This is for DAY1

--- DSA/Daksh_Mehrotra/1.py
+++ DSA/Daksh_Mehrotra/1.py
@@ -1,9 +1,16 @@
-def is_class_cancelled(threshold, arrival_times):
-    on_time_students = sum(1 for time in arrival_times if time <= 0)
-    if on_time_students < threshold:
-        return "YES"
-    else:
-        return "NO"
-threshold = 3
-arrival_times = [-1, -3, 4, 2]
-print(is_class_cancelled(threshold, arrival_times)) 
+def birthday_chocolate(candy, day, month):
+    n = len(candy)
+    count = 0
+    current_sum = sum(candy[:month])
+    if current_sum == day:
+        count += 1
+    for i in range(month, n):
+        current_sum = current_sum - candy[i - month] + candy[i]
+        if current_sum == day:
+            count += 1
+    return count
+candy = [2, 2, 1, 3, 2]
+day = 4
+month = 2
+result = birthday_chocolate(candy, day, month)
+print(result)  
