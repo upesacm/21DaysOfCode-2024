46bdca4194db8278252b23176fa4ea12efa922be
Manya Mangla <165951488+manya1604@users.noreply.github.com>
2024-06-23 23:45:52 +0530

    Update day2_dsa_ques1.java

--- DSA/manya mangla/day2_dsa_ques1.java
+++ DSA/manya mangla/day2_dsa_ques1.java
@@ -1,48 +1,29 @@
-import java.util.Arrays;
-import java.util.TreeSet;
+public class ClassCancellation {
 
-public class PlayerRanking {
-
-    public static int[] climbingLeaderboard(int[] leaderboard, int[] playerScores) {
-        // Use a TreeSet to store unique leaderboard scores in descending order
-        TreeSet<Integer> uniqueScores = new TreeSet<>((a, b) -> b - a);
-        for (int score : leaderboard) {
-            uniqueScores.add(score);
-        }
-
-        // Convert TreeSet to array for binary search
-        Integer[] uniqueScoresArray = uniqueScores.toArray(new Integer[0]);
-
-        int[] ranks = new int[playerScores.length];
-        int n = uniqueScoresArray.length;
-
-        // Binary search to find rank for each player score
-        for (int i = 0; i < playerScores.length; i++) {
-            int score = playerScores[i];
-            int pos = Arrays.binarySearch(uniqueScoresArray, score, (a, b) -> b - a);
-
-            if (pos >= 0) {
-                // Found exact score in leaderboard
-                ranks[i] = pos + 1;
-            } else {
-                // Score not found, calculate the rank as the position to insert + 1
-                pos = -(pos + 1);
-                ranks[i] = pos + 1;
+    public static String shouldCancelClass(int threshold, int[] arrivalTimes) {
+        int onTimeCount = 0;
+        
+        // Count number of on-time arrivals
+        for (int time : arrivalTimes) {
+            if (time <= 0) {  // non-positive indicates on-time
+                onTimeCount++;
             }
         }
-
-        return ranks;
+        
+        // Determine if class should be cancelled
+        if (onTimeCount < threshold) {
+            return "YES";
+        } else {
+            return "NO";
+        }
     }
 
     public static void main(String[] args) {
-        int[] leaderboard = {100, 90, 90, 80};
-        int[] playerScores = {70, 80, 105};
-
-        int[] ranks = climbingLeaderboard(leaderboard, playerScores);
-
-        // Output: [4, 3, 1]
-        for (int rank : ranks) {
-            System.out.println(rank);
-        }
+        int threshold = 3;
+        int[] arrivalTimes = {-1, -3, 4, 2};
+        
+        // Output: YES
+        System.out.println(shouldCancelClass(threshold, arrivalTimes));
     }
 }
+
