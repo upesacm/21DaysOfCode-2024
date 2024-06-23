9f5da9daf48266c11aced09bef4c6de1c9750235
DeeptanshNagar <diptanshnagar@gmail.com>
2024-06-24 03:08:32 +0530

    Update question1.java

--- DSA/Deeptansh-nagarr/day 3/question1.java
+++ DSA/Deeptansh-nagarr/day 3/question1.java
@@ -1,27 +1,23 @@
-import java.util.HashSet;
-import java.util.Set;
-
-public class upes21daysofcodeACM {
-    
+public class Main {
     public static String isPangram(String sentence) {
+        boolean[] alphabet = new boolean[26];
         sentence = sentence.toLowerCase();
-        Set<Character> alphabet = new HashSet<>();
-        for (char c = 'a'; c <= 'z'; c++) {
-            alphabet.add(c);
-        }
-        Set<Character> lettersInSentence = new HashSet<>();
+
         for (char c : sentence.toCharArray()) {
-            if (Character.isLetter(c)) {
-                lettersInSentence.add(c);
+            if (c >= 'a' && c <= 'z') {
+                alphabet[c - 'a'] = true;
             }
         }
-        if (lettersInSentence.containsAll(alphabet)) {
-            return "complete";
-        } else {
-            return "incomplete";
+
+        for (boolean b : alphabet) {
+            if (!b) {
+                return "incomplete";
+            }
         }
+
+        return "complete";
     }
-    
+
     public static void main(String[] args) {
         String sentence = "The quick brown fox jumps over the lazy dog";
         System.out.println(isPangram(sentence));
