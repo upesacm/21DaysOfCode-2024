c4e4aed2fee3f06444d4281a5dd9ef020d0afa33
Aanidhay Aggarwal <115068672+Aanidhay@users.noreply.github.com>
2024-06-24 23:55:39 +0530

    Update Day4_q1.java

--- DSA/Aanidhay Aggarwal/Day4_q1.java
+++ DSA/Aanidhay Aggarwal/Day4_q1.java
@@ -1,1 +1,38 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
+public class PalindromicSubstrings {
+    public static boolean isPalindrome(String substring) {
+        int start = 0;
+        int end = substring.length() - 1;
+        while (start < end) {
+            if (substring.charAt(start)!= substring.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    public static int countPalindromicSubstrings(String s, int length) {
+        Set<String> substrings = new HashSet<>();
+        for (int i = 0; i <= s.length() - length; i++) {
+            String substring = s.substring(i, i + length);
+            if (isPalindrome(substring)) {
+                substrings.add(substring);
+            }
+        }
+        return substrings.size();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = scanner.next();
+        System.out.print("Enter the length of the substrings: ");
+        int length = scanner.nextInt();
+        System.out.println("The number of distinct palindromic substrings is: " + countPalindromicSubstrings(s, length));
+    }
+}
