46b2ebd8b094fd78ffcc337c694609ac28fe9442
Yash gupta <76024829+yashgupta13@users.noreply.github.com>
2024-06-25 16:42:31 +0530

    Update Day_5_Q2.java

--- DSA/Yash_Gupta/Day_5_Q2.java
+++ DSA/Yash_Gupta/Day_5_Q2.java
@@ -2,18 +2,15 @@
 public class Day_5_Q2 {
 
     public static boolean areIdentical(Node head1, Node head2) {
-        //creating a copy of both heads
-        Node current1 = head1;
-        Node current2 = head2;
         //traversing both the list and comparing each node
-        while (current1 != null && current2 != null) {
-            if (current1.value != current2.value) {
+        while (head1 != null && head2 != null) {
+            if (head1.value != head2.value) {
                 return false;
             }
-            current1 = current1.next;
-            current2 = current2.next;
+            head1 = head1.next;
+            head2 = head2.next;
         }
-        return (current1 == null && current2 == null);//checking if both the pointers have reached the end
+        return (head1 == null && head2 == null);//checking if both the pointers have reached the end
     }
 
     public static void main(String[] args) {
@@ -21,7 +18,6 @@ public class Day_5_Q2 {
         Node head1 = new Node(1);
         head1.next = new Node(2);
         head1.next.next = new Node(3);
-        head1.next.next.next = new Node(4);
 
         Node head2 = new Node(1);
         head2.next = new Node(2);
