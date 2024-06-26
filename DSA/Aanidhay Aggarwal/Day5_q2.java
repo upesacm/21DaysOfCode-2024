b52ec8d81d9b1eb469cd1a3db6ebbb7573bdd583
Aanidhay Aggarwal <115068672+Aanidhay@users.noreply.github.com>
2024-06-26 11:34:52 +0530

    Update Day5_q2.java

--- DSA/Aanidhay Aggarwal/Day5_q2.java
+++ DSA/Aanidhay Aggarwal/Day5_q2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class LinkedList {
     Node head;
 
@@ -11,19 +13,32 @@ public class LinkedList {
         }
     }
 
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
     public boolean areIdentical(LinkedList list1, LinkedList list2) {
         Node node1 = list1.head;
         Node node2 = list2.head;
 
-        while (node1!= null && node2!= null) {
-            if (node1.data!= node2.data) {
+        while (node1 != null && node2 != null) {
+            if (node1.data != node2.data) {
                 return false;
             }
             node1 = node1.next;
             node2 = node2.next;
         }
 
-        if (node1!= null || node2!= null) {
+        if (node1 != null || node2 != null) {
             return false;
         }
 
@@ -31,24 +46,26 @@ public class LinkedList {
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         LinkedList list1 = new LinkedList();
-        list1.head = new Node(1);
-        list1.head.next = new Node(2);
-        list1.head.next.next = new Node(3);
+        System.out.print("Enter the number of elements for list 1: ");
+        int n1 = scanner.nextInt();
+        System.out.println("Enter the elements for list 1:");
+        for (int i = 0; i < n1; i++) {
+            list1.addNode(scanner.nextInt());
+        }
 
         LinkedList list2 = new LinkedList();
-        list2.head = new Node(1);
-        list2.head.next = new Node(2);
-        list2.head.next.next = new Node(3);
-
-        LinkedList list3 = new LinkedList();
-        list3.head = new Node(1);
-        list3.head.next = new Node(2);
-        list3.head.next.next = new Node(4);
+        System.out.print("Enter the number of elements for list 2: ");
+        int n2 = scanner.nextInt();
+        System.out.println("Enter the elements for list 2:");
+        for (int i = 0; i < n2; i++) {
+            list2.addNode(scanner.nextInt());
+        }
 
         LinkedList comparator = new LinkedList();
 
-        System.out.println(comparator.areIdentical(list1, list2)? "identical" : "different"); // Output: identical
-        System.out.println(comparator.areIdentical(list1, list3)? "identical" : "different"); // Output: different
+        System.out.println(comparator.areIdentical(list1, list2) ? "identical" : "different");
     }
 }
