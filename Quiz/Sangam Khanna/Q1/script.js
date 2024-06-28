b634151c39c9824a24645a6950dd423dbbe43298
Tanay <133881515+tanayprabhakar@users.noreply.github.com>
2024-06-28 16:26:11 +0530

    Delete Quiz/Sangam Khanna directory

--- Quiz/Sangam Khanna/Q1/script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  var x = document.getElementById("navbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
