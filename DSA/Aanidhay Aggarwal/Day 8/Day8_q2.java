c33487d38abcb9d4f6b27f4066a52e705e8c1e97
Aanidhay Aggarwal <115068672+Aanidhay@users.noreply.github.com>
2024-06-28 22:15:45 +0530

    Update Day8_q2.java

--- DSA/Aanidhay Aggarwal/Day 8/Day8_q2.java
+++ DSA/Aanidhay Aggarwal/Day 8/Day8_q2.java
@@ -1,1 +1,47 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class StackUsingTwoQueues {
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public StackUsingTwoQueues() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q2.add(x);
+        while (!q1.isEmpty()) {
+            q2.add(q1.poll());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        if (q1.isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return q1.poll();
+    }
+
+    public int top() {
+        if (q1.isEmpty()) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return q1.peek();
+    }
+
+    public static void main(String[] args) {
+        StackUsingTwoQueues stack = new StackUsingTwoQueues();
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.top()); // Output: 2
+        System.out.println(stack.pop()); // Output: 2
+        System.out.println(stack.top()); // Output: 1
+    }
+}
