e0e078a65f399f8cb930d1d68243fd3ed8e46baa
Aanidhay Aggarwal <115068672+Aanidhay@users.noreply.github.com>
2024-06-28 22:17:10 +0530

    Update Day8_q2.java

--- DSA/Aanidhay Aggarwal/Day 8/Day8_q2.java
+++ DSA/Aanidhay Aggarwal/Day 8/Day8_q2.java
@@ -1,47 +1,50 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class StackUsingTwoQueues {
-    private Queue<Integer> q1;
-    private Queue<Integer> q2;
+class MyStack {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+    private int topElement;
 
-    public StackUsingTwoQueues() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+    public MyStack() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
     }
 
     public void push(int x) {
-        q2.add(x);
-        while (!q1.isEmpty()) {
-            q2.add(q1.poll());
-        }
-        Queue<Integer> temp = q1;
-        q1 = q2;
-        q2 = temp;
+        queue1.add(x);
+        topElement = x;
     }
-
     public int pop() {
-        if (q1.isEmpty()) {
-            System.out.println("Stack is empty");
-            return -1;
+
+        while (queue1.size() > 1) {
+            topElement = queue1.remove();
+            queue2.add(topElement);
         }
-        return q1.poll();
+
+        int poppedElement = queue1.remove();
+
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+
+        return poppedElement;
     }
 
     public int top() {
-        if (q1.isEmpty()) {
-            System.out.println("Stack is empty");
-            return -1;
-        }
-        return q1.peek();
+        return topElement;
+    }
+    
+    public boolean empty() {
+        return queue1.isEmpty();
     }
 
     public static void main(String[] args) {
-        StackUsingTwoQueues stack = new StackUsingTwoQueues();
+        MyStack stack = new MyStack();
         stack.push(1);
         stack.push(2);
-        System.out.println(stack.top()); // Output: 2
-        System.out.println(stack.pop()); // Output: 2
-        System.out.println(stack.top()); // Output: 1
+        System.out.println(stack.top());
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
     }
 }
