f0a5fcde25dd72d8f874d5463c6ed64c3eae24e2
Mradul-Lakhotiya <mradullakhotiya18@gmail.com>
2024-06-29 11:57:27 +0530

    Day-8(21DOC)

--- Quiz/MradulLakhotiya/Q1/script.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("nav-bar")            
-    if (x.className === "top-nav-bar") {
-        x.className += " responsive";
-    }
-    else {
-        x.className = "top-nav-bar";
-    }
-}
