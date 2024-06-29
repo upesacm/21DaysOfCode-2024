bf4da79339639ff58ee710122e338a09b176336d
Eyepatch <63046602+Aryansaini10@users.noreply.github.com>
2024-06-29 11:17:57 +0530

    Update q5.js

--- Web-Dev/Aryan Saini/Day 8/q5.js
+++ Web-Dev/Aryan Saini/Day 8/q5.js
@@ -1,1 +1,16 @@
 
+const coffeePrice = 5;
+const bagelPrice = 3;
+const soupPrice = 9;
+
+
+const coffeeQuantity = 1;
+const bagelQuantity = 2;
+const soupQuantity = 1;
+
+
+const cost = (coffeePrice * coffeeQuantity) + (bagelPrice * bagelQuantity) + (soupPrice * soupQuantity);
+
+
+document.getElementById("order-cost").innerHTML = "Total Cost: $" + cost;
+
