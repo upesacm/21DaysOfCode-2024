5347dd89f118de1418095dad3072f3d1f4b8061f
Aanidhay Aggarwal <115068672+Aanidhay@users.noreply.github.com>
2024-06-29 20:27:32 +0530

    Update Day9_q1.java

--- DSA/Aanidhay Aggarwal/Day 9/Day9_q1.java
+++ DSA/Aanidhay Aggarwal/Day 9/Day9_q1.java
@@ -1,1 +1,45 @@
+import java.util.Stack;
 
+public class MaxStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> maxStack;
+
+    public MaxStack() {
+        stack = new Stack<>();
+        maxStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (maxStack.isEmpty() || x >= maxStack.peek()) {
+            maxStack.push(x);
+        }
+    }
+
+    public int pop() {
+        if (stack.isEmpty()) {
+            return -1;  // or throw an exception
+        }
+        int top = stack.pop();
+        if (top == maxStack.peek()) {
+            maxStack.pop();
+        }
+        return top;
+    }
+
+    public int getMax() {
+        if (maxStack.isEmpty()) {
+            return -1;  // or throw an exception
+        }
+        return maxStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MaxStack stack = new MaxStack();
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.getMax());
+        stack.pop();
+        System.out.println(stack.getMax());
+    }
+}
