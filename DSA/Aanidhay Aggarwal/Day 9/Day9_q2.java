1675783a23c0d56ecbf05d01b8825228c5eae30e
Aanidhay Aggarwal <115068672+Aanidhay@users.noreply.github.com>
2024-06-29 20:35:00 +0530

    Update Day9_q2.java

--- DSA/Aanidhay Aggarwal/Day 9/Day9_q2.java
+++ DSA/Aanidhay Aggarwal/Day 9/Day9_q2.java
@@ -1,1 +1,24 @@
+import java.util.Stack;
 
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isBalanced("{[()]}"));  // Output: YES
+        System.out.println(isBalanced("([)]"));  // Output: NO
+    }
+
+    public static String isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return "NO";
+            }
+        }
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+}
