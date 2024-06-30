c98bf8d65f2c1be8f88c22521255bd408137cc23
Aanidhay Aggarwal <115068672+Aanidhay@users.noreply.github.com>
2024-06-30 16:57:17 +0530

    Update Day10_q2.java

--- DSA/Aanidhay-Aggarwal/Day-10/Day10_q2.java
+++ DSA/Aanidhay-Aggarwal/Day-10/Day10_q2.java
@@ -1,1 +1,32 @@
+import java.util.Stack;
 
+public class QueueUsingTwoStacks {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    public QueueUsingTwoStacks() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void enqueue(int element) {
+        stack1.push(element);
+    }
+
+    public int dequeue() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        QueueUsingTwoStacks queue = new QueueUsingTwoStacks();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+    }
+}
