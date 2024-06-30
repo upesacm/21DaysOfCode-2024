718f655a9360075646f7c5adf44b88efdbe85860
Aanidhay Aggarwal <115068672+Aanidhay@users.noreply.github.com>
2024-06-30 16:54:06 +0530

    Update Day10_q1.java

--- DSA/Aanidhay-Aggarwal/Day-10/Day10_q1.java
+++ DSA/Aanidhay-Aggarwal/Day-10/Day10_q1.java
@@ -1,1 +1,25 @@
+import java.util.Queue;
+import java.util.LinkedList;
 
+public class Main {
+    public static Queue<Integer> reverseQueue(Queue<Integer> queue) {
+        if (queue.isEmpty()) {
+            return queue;
+        }
+        int front = queue.poll();
+        queue = reverseQueue(queue);
+        queue.add(front);
+        return queue;
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+
+        queue = reverseQueue(queue);
+
+        System.out.println("Reversed queue: " + queue);
+    }
+}
