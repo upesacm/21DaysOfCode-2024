96571938da34a09f120609ff14d312cdb06b33dd
DeeptanshNagar <diptanshnagar@gmail.com>
2024-07-02 11:46:44 +0530

    Update question2.java

--- DSA/Deeptansh-nagarr/day 11/question2.java
+++ DSA/Deeptansh-nagarr/day 11/question2.java
@@ -1,66 +1,70 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PriorityQueue {
+class pq {
     private List<Integer> heap;
-
-    public PriorityQueue() {
-        heap = new ArrayList<>();
+    public pq() 
+    {
+        this.heap = new ArrayList<>() ;
     }
-
-    public void insert(int value) {
-        heap.add(value);
-        int index = heap.size() - 1;
-        while (index > 0) {
-            int parentIndex = (index - 1) / 2;
-            if (heap.get(parentIndex) <= heap.get(index)) {
-                break;
-            }
-            swap(parentIndex, index);
-            index = parentIndex;
-        }
+    public void ins(int val) {
+        heap.add(val);
+        siftup(heap.size() - 1) ;
     }
-
-    public int extractMin() {
-        if (heap.isEmpty()) {
-            throw new RuntimeException("Priority queue is empty");
+    public int exmin() {
+        if (heap.isEmpty()) 
+        {
+            throw new IllegalStateException("Priority queue is empty");
         }
         int min = heap.get(0);
-        heap.set(0, heap.get(heap.size() - 1));
-        heap.remove(heap.size() - 1);
-        heapify(0);
+        int lastel = heap.remove(heap.size() - 1);
+        if (!heap.isEmpty()) 
+        {
+            heap.set(0, lastel);
+            siftdn(0);
+        }
+
         return min;
     }
-
-    private void heapify(int index) {
-        int smallest = index;
-        int left = 2 * index + 1;
-        int right = 2 * index + 2;
-
-        if (left < heap.size() && heap.get(left) < heap.get(smallest)) {
-            smallest = left;
-        }
-
-        if (right < heap.size() && heap.get(right) < heap.get(smallest)) {
-            smallest = right;
-        }
-
-        if (smallest != index) {
-            swap(smallest, index);
-            heapify(smallest);
+    private void siftup(int index) {
+        int pind = (index - 1) / 2;
+        while (index > 0 && heap.get(index) < heap.get(pind)) {
+            swap(index, pind);
+            index = pind;
+            pind = (index - 1) / 2;
         }
     }
-
-    private void swap(int i, int j) {
+    private void siftdn(int index) {
+        int minind = index;
+        int lc = 2 * index + 1;
+        int rc = 2 * index + 2;
+        if (lc < heap.size() && heap.get(lc) < heap.get(minind)) {
+          minind = lc;
+        }
+        if (rc < heap.size() && heap.get(rc) < heap.get(minind)) {
+          minind = rc;
+        }
+        if (index != minind) {
+            swap(index, minind);
+            siftdn(minind);
+        }
+    }
+    private void swap(int i, int j) 
+    {
         int temp = heap.get(i);
         heap.set(i, heap.get(j));
         heap.set(j, temp);
     }
-
-    public static void main(String[] args) {
-        PriorityQueue priorityQueue = new PriorityQueue();
-        priorityQueue.insert(4);
-        priorityQueue.insert(2);
-        System.out.println(priorityQueue.extractMin());
-        System.out.println(priorityQueue.extractMin());
+    public boolean isEmpty() 
+     {
+        return heap.isEmpty();
+    }
+    public static void main(String[] args) 
+    {
+        pq ob = new pq();
+        ob.ins(4);
+        ob.ins(2);
+        System.out.println(ob.exmin()); 
+        System.out.println(ob.exmin()) ; 
     }
 }
