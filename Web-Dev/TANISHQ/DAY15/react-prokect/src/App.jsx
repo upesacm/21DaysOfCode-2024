e36218a3f826d1757cf62447ae8f03214a41e4d4
TanishqGupta1234 <vandanabhay1997@gmail.com>
2024-07-06 00:21:03 +0530

    MY HARDWORK OF DAY 15

---
+++ Web-Dev/TANISHQ/DAY15/react-prokect/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ButtonClicker from "./assets/Components";
+import LikeButton from "./assets/Components/index1";
+import TrafficLight from "./assets/Components/index3";
+
+
+function App() {
+
+  return (
+    <>
+
+    <ButtonClicker/>
+    <LikeButton/>
+    <TrafficLight/>
+
+
+
+    
+    
+    
+    </>
+
+  )
+}
+
+export default App
