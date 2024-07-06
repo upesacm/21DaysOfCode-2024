ff37b2163c8d251486fb61a1864fa31266c24a0c
Vyom Singhal <126981737+Vilsium@users.noreply.github.com>
2024-07-06 23:53:06 +0530

    Add files via upload

---
+++ DSA/Vyom-Singhal/Day16/Day16-q2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void inOrderTraversal(TreeNode* root) {
+    if(root == NULL) return;
+    inOrderTraversal(root->left);
+    cout << root->val << " ";
+    inOrderTraversal(root->right);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+    inOrderTraversal(root);
+}
