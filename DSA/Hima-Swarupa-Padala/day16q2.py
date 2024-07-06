07d5f4ec15d4e314f29eb7fd34fb46d7187408a6
Hima Swarupa Padala <164299178+Himaswarupa@users.noreply.github.com>
2024-07-06 14:38:29 +0530

    day16q2.py

--- DSA/Hima-Swarupa-Padala/day16q2.py
+++ DSA/Hima-Swarupa-Padala/day16q2.py
@@ -1,22 +1,18 @@
 class TreeNode:
-
     def __init__(self, value):      
         self.value = value
         self.left = None
         self.right = None
 
 def inorder_traversal(root):
-
     result = []
 
-    def traverse(node):
-        
+    def traverse(node):       
         if node:
             traverse(node.left)
             result.append(node.value)
             traverse(node.right)
     traverse(root)
-
     return result
 
 root = TreeNode(1)
@@ -26,5 +22,4 @@ root.left.left = TreeNode(4)
 root.left.right = TreeNode(5)
 
 result = inorder_traversal(root)
-
-print(result)  
+print(result) 
