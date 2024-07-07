e865647aa3cdb92d8fa1fd44c2f3095a3a61aa1d
Eyepatch <63046602+Aryansaini10@users.noreply.github.com>
2024-07-07 11:44:44 +0530

    Create q2.cday 16

---
+++ DSA/Aryan-Saini/Day 16/q2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* createNode(int val) {
+    struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    newNode->val = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+void inorderTraversal(struct TreeNode* root) {
+    if (root == NULL)
+        return;
+    
+    inorderTraversal(root->left);
+    printf("%d ", root->val);
+    inorderTraversal(root->right);
+}
+
+int main() {
+
+    struct TreeNode* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Inorder Traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    free(root->left->left);
+    free(root->left->right);
+    free(root->left);
+    free(root->right);
+    free(root);
+
+    return 0;
+}
