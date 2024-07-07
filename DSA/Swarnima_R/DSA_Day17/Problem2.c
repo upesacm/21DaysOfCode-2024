710e9e13f25c5446fe172a03294e738187a4e00e
Swarnima <124855325+oringejooz@users.noreply.github.com>
2024-07-08 01:47:45 +0530

    Changes to be committed: 	new file:   DSA/Swarnima_R/DSA_Day17/Problem1.c 	new file:   DSA/Swarnima_R/DSA_Day17/Problem1.exe 	new file:   DSA/Swarnima_R/DSA_Day17/Problem2.c 	new file:   DSA/Swarnima_R/DSA_Day17/Problem2.exe

---
+++ DSA/Swarnima_R/DSA_Day17/Problem2.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* newNode(int data) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL) return newNode(val);
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else if (val > root->data)
+        root->right = insert(root->right, val);
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    struct Node* root = newNode(4);
+    root->left = newNode(2);
+    root->right = newNode(7);
+    root->left->left = newNode(1);
+    root->left->right = newNode(3);
+
+    printf("Original BST: ");
+    inorder(root);
+    printf("\n");
+
+    int val = 5;
+    root = insert(root, val);
+
+    printf("Updated BST: ");
+    inorder(root);
+    printf("\n");
+
+    return 0;
+}
