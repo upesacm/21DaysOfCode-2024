6874f762c7176bbaa1bb3f12bf2d905e96e9385c
Hima Swarupa Padala <164299178+Himaswarupa@users.noreply.github.com>
2024-07-07 17:40:23 +0530

    Update day17q2.py

--- DSA/Hima-Swarupa-Padala/day17q2.py
+++ DSA/Hima-Swarupa-Padala/day17q2.py
@@ -1,4 +1,5 @@
 class TreeNode:
+    
     def __init__(self, val):
         self.val = val
         self.left = None
@@ -6,16 +7,15 @@ class TreeNode:
 
 def insert_into_BST(root, value):
     if not root:
-        return TreeNode(value)  # Insert the first node if root is None
+        return TreeNode(value)  
 
     if value > root.val:
-        root.right = insert_into_BST(root.right, value)  # Insert into right subtree
+        root.right = insert_into_BST(root.right, value)  
     elif value < root.val:
-        root.left = insert_into_BST(root.left, value)  # Insert into left subtree
+        root.left = insert_into_BST(root.left, value) 
 
     return root
 
-# Example usage
 root = TreeNode(4)
 root.left = TreeNode(2)
 root.right = TreeNode(7)
@@ -25,7 +25,6 @@ root.left.right = TreeNode(3)
 new_value = 5
 root = insert_into_BST(root, new_value)
 
-# Inorder traversal to verify the updated BST
 def inorder_traversal(node):
     if not node:
         return
