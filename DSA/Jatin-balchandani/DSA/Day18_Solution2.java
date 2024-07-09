1ed51c55db17db759c7c81ec9acf7ea36aaf6808
Jatin kumar Balchandani <89150120+jatinavi@users.noreply.github.com>
2024-07-09 11:28:30 +0530

    Add files via upload

---
+++ DSA/Jatin-balchandani/DSA/Day18_Solution2.java
@@ -0,0 +1,63 @@
+class HuffmanNode
+{
+    char character;
+    int frequency;
+    HuffmanNode left;
+    HuffmanNode right;
+
+    HuffmanNode(char character, int frequency)
+    {
+
+        this.character = character;
+        this.frequency = frequency;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Day18_Solution2
+{
+    HuffmanNode root;
+
+    Day18_Solution2(HuffmanNode root)
+    {
+        this.root = root;
+    }
+    public String decode(String binaryString)
+    {
+        StringBuilder decodedText = new StringBuilder();
+        HuffmanNode currentNode = root;
+        for (int i = 0; i < binaryString.length(); i++)
+        {
+            char bit = binaryString.charAt(i);
+            if (bit == '0') {
+                currentNode = currentNode.left;
+            } else if (bit == '1') {
+                currentNode = currentNode.right;
+            }
+            if (currentNode.left == null && currentNode.right == null)
+            {
+                decodedText.append(currentNode.character);
+                currentNode = root;
+            }
+        }
+        return decodedText.toString();
+    }
+    public static void main(String[] args)
+    {
+        HuffmanNode nodeA = new HuffmanNode('A', 3);
+        HuffmanNode nodeB = new HuffmanNode('B', 1);
+        HuffmanNode nodeC = new HuffmanNode('C', 1);
+        HuffmanNode internalNode = new HuffmanNode('*', 2);
+        internalNode.left = nodeB;
+        internalNode.right = nodeC;
+
+        HuffmanNode root = new HuffmanNode('*', 5);
+        root.left = nodeA;
+        root.right = internalNode;
+        Day18_Solution2 huffmanTree = new Day18_Solution2(root);
+        String binaryString = "1001011";
+        String decodedText = huffmanTree.decode(binaryString);
+        System.out.println("Decoded text: " + decodedText);
+    }
+}
